e38465130012b8b6adca2e6d918b4f624a314420
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-06-07 18:37:50 +0100

    filtered jobs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,13 @@ export class AppComponent implements OnInit, OnDestroy {
   levels: string[] = [];
   languages: string[] = [];
   tools: string[] = [];
+
+  get showSearchBar(): boolean {
+    return !!this.roles.length ||
+          !!this.levels.length ||
+       !!this.languages.length ||
+          !!this.tools.length
+  }
   protected sub!: Subscription;
   constructor(private jobService: JobService) { }
 
@@ -37,20 +44,65 @@ export class AppComponent implements OnInit, OnDestroy {
     )
   }
 
+  private filterJobs(): void {
+    this.filteredJobs = [...this.originalJobs];
+    if (!!this.roles.length) {
+      this.filteredJobs = this.filteredJobs.filter(r => this.roles.includes(r.role));
+    }
+    if (!!this.levels.length) {
+      this.filteredJobs = this.filteredJobs.filter(l => this.levels.includes(l.level));
+    }
+    if (!!this.languages.length) {
+      this.filteredJobs = this.filteredJobs.filter(l => l.languages.some(lg => this.languages.includes(lg)));
+    }
+    if (!!this.tools.length) {
+      this.filteredJobs = this.filteredJobs.filter(t => t.tools.some(tl => this.tools.includes(tl)) );
+    }
+  }
+
   addRole(role: string): void {
     this.roles.push(role);
-    this.roles = [...new Set(this.roles)]
+    this.roles = [...new Set(this.roles)];
+    this.filterJobs();
   }
   addLevel(level: string): void {
     this.levels.push(level);
-    this.levels = [...new Set(this.levels)]
+    this.levels = [...new Set(this.levels)];
+    this.filterJobs();
   }
   addLanguage(language: string): void {
     this.languages.push(language);
-    this.languages = [...new Set(this.languages)]
+    this.languages = [...new Set(this.languages)];
+    this.filterJobs();
   }
   addTool(tool: string): void {
     this.tools.push(tool);
-    this.tools = [...new Set(this.tools)]
+    this.tools = [...new Set(this.tools)];
+    this.filterJobs();
+  }
+
+  removeRole(role: string): void {
+    this.roles = this.roles.filter(r => r !== role);
+    this.filterJobs();
+  }
+  removeLevel(level: string): void {
+    this.levels = this.levels.filter(l => l !== level);
+    this.filterJobs();
+  }
+  removeLanguage(language: string): void {
+    this.languages = this.languages.filter(l => l !== language);
+    this.filterJobs();
+  }
+  removeTool(tool: string): void {
+    this.tools = this.tools.filter(t => t !== tool);
+    this.filterJobs();
+  }
+
+  clearFilters(): void {
+    this.roles = [];
+    this.levels = [];
+    this.languages = [];
+    this.tools = [];
+    this.filterJobs();
   }
 }
